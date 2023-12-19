16771ee2fefeb31ac944474e0d0f2a2fa52a1d5c
inquisitive-gunjon <gunjon.cse@gmail.com>
2023-12-19 13:02:21 +0600

    create- admin login presentation layer

---
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -0,0 +1,16 @@
+
+import 'package:hospital_riverpod/features/auth/presentation/providers/state/auth_notifire.dart';
+import 'package:hospital_riverpod/features/auth/presentation/providers/state/auth_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../domain/providers/auth_repository_provider.dart';
+import '../../domain/repositories/authentication_repository.dart';
+
+
+final authStateNotifierProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
+
+  final AuthenticationRepository authenticationRepository = ref.watch(authRepositoryProvider);
+  return AuthNotifier(
+    authRepository: authenticationRepository,
+  );
+},
+);

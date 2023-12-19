84a3d52b02e46e329567a0ab6107be1a3aa5cd7a
inquisitive-gunjon <gunjon.cse@gmail.com>
2023-12-19 15:08:28 +0600

    config- app_route, auth_guard and screens

---
+++ lib/features/blog/presentation/screens/bloglist_screen.dart
@@ -0,0 +1,22 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+
+@RoutePage()
+class BlogListScreen extends ConsumerStatefulWidget {
+  const BlogListScreen({super.key});
+
+  @override
+  ConsumerState<BlogListScreen> createState() => _BlogListScreenState();
+}
+
+class _BlogListScreenState extends ConsumerState<BlogListScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
